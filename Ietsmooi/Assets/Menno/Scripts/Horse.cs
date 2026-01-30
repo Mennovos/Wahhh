2f0ee2d76762ad94131feb7adbe0e7028f0fbd5f
Mennovos <mennodvos@gmail.com>
2026-01-30 21:07:39 +0100

    Fix: Way Smoother transition

--- Ietsmooi/Assets/Menno/Scripts/Horse.cs
+++ Ietsmooi/Assets/Menno/Scripts/Horse.cs
@@ -6,15 +6,26 @@ public class Horse : MonoBehaviour
     [SerializeField] private GameObject horse;
     private PlayerScript playerScript;
     private bool Inrange;
+    private float range = 3f;
     public bool hasHorse;
     private void Start()
     {
         playerScript = FindFirstObjectByType<PlayerScript>();
         horse.SetActive(false);
     }
-    private void OnTriggerEnter(Collider other)
+    private void Update()
     {
-        Inrange = true;
+        if (Vector3.Distance(transform.position, player.transform.position) <= range)
+        {
+            Inrange = true;
+            // Player is in range
+        }
+        else
+        {
+            Inrange = false;
+            // Player is out of range
+        }
+
         if (playerScript.isInteracting == true && Inrange == true)
         {
             hasHorse = true;
