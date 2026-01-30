d08291c516b1cd6ba2e60431406a18e3bbcbad0f
Felix <felixnieuwenhuijsen@gmail.com>
2026-01-30 16:42:51 +0100

    Fix: edited item hold positioning

--- Ietsmooi/Assets/Felix/Scripts/PlayerHoldScript.cs
+++ Ietsmooi/Assets/Felix/Scripts/PlayerHoldScript.cs
@@ -4,6 +4,8 @@ public class PlayerHoldScript : MonoBehaviour
 {
     [SerializeField] private Transform hand;
     public InventoryItem[] allItems;
+    public GameObject itemHolding;
+    public int itemHoldingIndex;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,11 +23,13 @@ public class PlayerHoldScript : MonoBehaviour
 
     private void AddToInventory(InventoryItem i)
     {
-        GameObject a = Instantiate(i.obj, Vector3.zero, Quaternion.Euler(0f, 0f, 0f));
+        GameObject a = Instantiate(i.obj, Vector3.zero + i.posOffset, Quaternion.identity * i.rotOffset);
         a.transform.SetParent(hand);
 
         a.transform.localPosition = Vector3.zero;
         Debug.Log(a);
+
+        a.SetActive(false);
     }
 }
 
