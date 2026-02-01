2c8a79ee2a85d013614f3bc3fe1ccc64f2b52567
Mennovos <mennodvos@gmail.com>
2026-02-01 15:06:26 +0100

    Feat: navmesh

--- Ietsmooi/Assets/Menno/Scripts/Npc.cs
+++ Ietsmooi/Assets/Menno/Scripts/Npc.cs
@@ -1,15 +1,14 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Npc : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private List<Transform> Npclocations;
+    private void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
     }
